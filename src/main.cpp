974f6d9e7f3ba5b8269ca4f16af362de4d506401
Andriy Lysnevych <andriy.lysnevych@gmail.com>
2017-04-27 15:31:09 +0300

    Ability to specify port in command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <udp_proxy/udp_proxy.h>
 
-int main() {
+int main(int argc, const char * const argv[]) {
+    unsigned short port = 6000;
+    if (argc >= 2) {
+        try {
+            port = std::stoul(argv[1]);
+        } catch(...) {
+        }
+    }
+
     try {
         boost::asio::io_service ioService;
-        UdpProxy::Server server(ioService, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 10013));
+        UdpProxy::Server server(ioService, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port));
+        std::cout << "Running on port " << port << std::endl;
         ioService.run();
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
